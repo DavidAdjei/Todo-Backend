80e7821c313cd91712161caeca6c2a516b500b5a
DavidAdjei <97539347+DavidAdjei@users.noreply.github.com>
2024-09-15 12:32:37 +0000

    trying session again

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
-const { hashPassword, comparePassword, generateToken } = require('../helpers/userHelpers');
+const db = require("../config/db");
+const { hashPassword, comparePassword } = require('../helpers/userHelpers');
 const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
@@ -36,13 +37,9 @@ exports.createUser = async (req, res) => {
             });
         }
 
-        const token = generateToken(newUser[0].id , 'auth', '3d' );
-
-        res.cookie('authToken', token, {
-            httpOnly: true, 
-            sameSite: 'none',
-            secure: process.env.NODE_ENV === 'production', 
-            maxAge: 1000 * 60 * 60 * 24 * 3,
+        req.session.user = newUser.id;
+        return res.status(200).json({
+            user: newUser[0]
         });
 
     } catch (err) {
@@ -55,7 +52,7 @@ exports.createUser = async (req, res) => {
 
 
 exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
+    const {email, password } = req.body;
 
     if (!email || !password) {
         return res.status(401).json({
@@ -77,17 +74,11 @@ exports.loginUser = async (req, res) => {
         const match = await comparePassword(password, hashed);
         if (!match) {
             return res.status(404).json({
-                error: "Incorrect credentials"
-            });
+                error: "Inncorrect credentials"
+            })
         }
         
-        const token = generateToken(existingUser[0].id , 'auth', '3d' );
-        res.cookie('authToken', token, {
-            httpOnly: true, 
-            secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'none',
-            maxAge: 1000 * 60 * 60 * 24 * 3,
-        });
+        req.session.user = existingUser.id;
         return res.status(200).json({
             user: existingUser[0]
         });
@@ -100,30 +91,15 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.isAuth = (req, res, next) => {
-    const token = req.cookies.authToken;
-    console.log(req.cookies);
-
-    if (!token) {
-        return res.status(403).json({ error: "Not authenticated" });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (decoded.type !== "auth") {
-            return res.status(400).json({ error: "Invalid token" });
-        }
-        req.userId = decoded.id; 
-        next();
-    } catch (err) {
-        console.error("Token verification error:", err);
-        return res.status(403).json({ error: "Not authenticated" });
-    }
+  if (req.session.user) {
+    next();
+  } else {
+    return res.status(403).json({ error: "Not authenticated" });
+  }
 };
 
 exports.authenticate = async (req, res) => {
-    const id = req.userId;
-
-    console.log(req.userId);
+    const id = req.session.user;
     try {
         const existingUser = await User.findUserById(id);
         if (existingUser.length === 0) {
@@ -135,22 +111,8 @@ exports.authenticate = async (req, res) => {
             user: existingUser[0]
         });
     } catch (err) {
-        console.error("Error fetching user:", err);
         return res.status(500).json({
             error: "Internal Server Error"
         });
     }
 };
-
-exports.logout = (req, res) => {
-    res.cookie('authToken', '', {
-        httpOnly: true, 
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
-        expires: new Date(0) 
-    });
-    return res.status(200).json({
-        message: "Successfully logged out"
-    });
-};
-
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const session = require("express-session");
+const Store = require('express-mysql-session')(session);
 const cookieParser = require("cookie-parser");
 const authRoute = require('./routes/auth');
 const todoRoute = require('./routes/todo');
@@ -10,10 +12,34 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+const options = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+}
+
+const store = new Store(options);
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use(
+  session({
+    secret: 'blahblah',
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24 * 3,
+    },
+  })
+);
 
 app.set('trust proxy', 1);
 
